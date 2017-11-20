6a90e11fc758ef9c3c7df39ca3ee0595f3d32305
Ana Cuevas Bravo <acuevas@gamma07.aulas.gsyc.urjc.es>
2017-11-20 10:38:43 +0100

    argumentos y manda invite y bye

--- client.py
+++ client.py
@@ -5,23 +5,27 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
 
-# Cliente UDP simple.
+if len(sys.argv) != 3:
+    sys.exit('Usage: python3 client.py method receiver@IP:SIPport')
 
-# Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+Method = sys.argv[1];
+User = sys.argv[2].split('@')[0]
+Dir = sys.argv[2].split('@')[1]
+SERVER = Dir.split(':')[0]
+PORT = int(Dir.split(':')[1])
+
 
 # Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+Message = Method + ' sip:' + sys.argv[2] + ' SIP/2.0' + '\r\n\r\n'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     my_socket.connect((SERVER, PORT))
 
-    print("Enviando: " + LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    my_socket.send(bytes(Message, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
 
     print('Recibido -- ', data.decode('utf-8'))
