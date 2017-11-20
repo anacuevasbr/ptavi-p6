8f7857a8b1c10db76b32ec278efec168215253bc
Ana Cuevas Bravo <acuevas@gamma07.aulas.gsyc.urjc.es>
2017-11-20 10:38:09 +0100

    argumentos y error 405

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import sys
 
 
 class EchoHandler(socketserver.DatagramRequestHandler):
@@ -18,14 +19,30 @@ class EchoHandler(socketserver.DatagramRequestHandler):
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
-            print("El cliente nos manda " + line.decode('utf-8'))
+            Message = line.decode('utf-8').split(' ')[0]
+            if Message == 'INVITE':
+                pass
+            elif Message == 'BYE':
+                pass
+            elif Message == 'ACK':
+                pass
+            else:
+                self.wfile.write(b"SIP/2.0 405 Method Not Allowed \r\n\r\n")
+            print("El cliente nos manda " + Message)
 
             # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
 
 if __name__ == "__main__":
+    
+    if len(sys.argv) != 4:
+        sys.exit('Usage: python3 server.py IP port audio_file')
+
+    IP = sys.argv[1]
+    PORT = int(sys.argv[2])
+    audio = sys.argv[3]
     # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
-    print("Lanzando servidor UDP de eco...")
+    serv = socketserver.UDPServer((IP, PORT), EchoHandler)
+    print("Listening...")
     serv.serve_forever()
