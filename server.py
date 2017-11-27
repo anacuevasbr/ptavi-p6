bfeb622dd182664cb67a5e24ca920aa86f15e17d
Ana Cuevas Bravo <acuevas@gamma07.aulas.gsyc.urjc.es>
2017-11-27 10:24:37 +0100

    pep8

--- server.py
+++ server.py
@@ -21,11 +21,12 @@ class EchoHandler(socketserver.DatagramRequestHandler):
             Message = line.decode('utf-8').split(' ')
             Method = str(Message[0])
             if Method != '':
-                if len(Message) == 3 and str(Message[2].split("/")[0]) == 'SIP':
+                if len(Message) == 3 and
+                    str(Message[2].split("/")[0]) == 'SIP':
                     if Method == 'INVITE':
-                        line = ('SIP/2.0 100 Trying \r\n\r\n'
-                                + 'SIP/2.0 180 Ringing \r\n\r\n'
-                                + 'SIP/2.0 200 OK \r\n\r\n')
+                        line = ('SIP/2.0 100 Trying \r\n\r\n' +
+                                'SIP/2.0 180 Ringing \r\n\r\n' +
+                                'SIP/2.0 200 OK \r\n\r\n')
                         self.wfile.write(bytes(line, 'utf-8'))
                     elif Method == 'BYE':
                         line = ('SIP/2.0 200 OK \r\n\r\n')
@@ -35,7 +36,8 @@ class EchoHandler(socketserver.DatagramRequestHandler):
                         order = "./mp32rtp -i 127.0.0.1 -p 23032 < " + audio
                         os.system(order)
                     else:
-                        self.wfile.write(b"SIP/2.0 405 Method Not Allowed \r\n\r\n")
+                        self.wfile.write(b"SIP/2.0 405 Method" +
+                                         "Not Allowed \r\n\r\n")
                     print("El cliente nos manda " + str(Message[0]))
                 else:
                     self.wfile.write(b"SIP/2.0 400 Bad Request \r\n\r\n")
@@ -45,13 +47,13 @@ class EchoHandler(socketserver.DatagramRequestHandler):
                 break
 
 if __name__ == "__main__":
-    
+
     if len(sys.argv) != 4:
         sys.exit('Usage: python3 server.py IP port audio_file')
 
     IP = sys.argv[1]
     PORT = int(sys.argv[2])
-    
+
     # Creamos servidor de eco y escuchamos
     serv = socketserver.UDPServer((IP, PORT), EchoHandler)
     print("Listening...")
