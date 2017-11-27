fa2a7b41aa13e91ccc9c679ed2c2ff3194b5779e
Ana Cuevas Bravo <acuevas@gamma07.aulas.gsyc.urjc.es>
2017-11-27 10:49:23 +0100

    comprobacion del mensaje para error 400

--- server.py
+++ server.py
@@ -13,16 +13,26 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     Echo server class
     """
 
+    def Valid(self, Message):
+        Val = False
+        Lon = len(Message) == 3
+        Sip = str(Message[2].split("/")[0]) == 'SIP'
+        Arroba = Message[1].find('@') != -1
+
+        if Lon and Sip and Arroba:
+            Val = True
+        return Val
+
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
+            print(line.decode('utf-8'))
             Message = line.decode('utf-8').split(' ')
             Method = str(Message[0])
             if Method != '':
-                if len(Message) == 3 and
-                    str(Message[2].split("/")[0]) == 'SIP':
+                if self.Valid(Message):
                     if Method == 'INVITE':
                         line = ('SIP/2.0 100 Trying \r\n\r\n' +
                                 'SIP/2.0 180 Ringing \r\n\r\n' +
@@ -41,7 +51,7 @@ class EchoHandler(socketserver.DatagramRequestHandler):
                     print("El cliente nos manda " + str(Message[0]))
                 else:
                     self.wfile.write(b"SIP/2.0 400 Bad Request \r\n\r\n")
-
+                    print("SIP/2.0 400 Bad Request \r\n\r\n")
             # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
